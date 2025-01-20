8d6bbff29ea6351c175245cf6857f99c69f5f9f0
arvind <arvind6599@gmail.com>
2025-01-20 23:58:01 +0100

    update

--- CONST.py
+++ CONST.py
@@ -1,11 +1,8 @@
-MODEL_NAMES = [
-    "gemma2:2b",
-    "qwen2.5:3b",
-    "exaone3.5:2.4b"
-]
+MODEL_NAMES = ["gemma2:2b", "qwen2.5:3b", "exaone3.5:2.4b"]
 
 PROMPT_LIBRARY = {
-    "DPI": """You are a helpful assistant specialized in gaming gear optimization, particularly in determining the best DPI settings for gaming. Your goal is to engage users in a series of well-structured questions with three multiple-choice answers to help them find an optimal DPI for their gaming setup.
+    "USER_PROMPT": "\nQuestion: ",
+    "DPI": """You are a helpful assistant specialized in gaming gear optimization, particularly in determining the best DPI settings for gaming. Your goal is to engage users in a series of well-structured questions along with four multiple-choice answers to help them find an optimal DPI for their gaming setup.
 
 **User Profile**:
 - **Game**: {game}
@@ -13,10 +10,11 @@ PROMPT_LIBRARY = {
 - **Keyboard**: Logitech G Pro X
 
 ### Guidelines:
-1. Begin by asking the user their **current DPI**. Include an option for "Not sure" if they don't know.
-2. Ask if they are satisfied with their current DPI. If they are, conclude the session by affirming their settings.
-3. If they are not satisfied, ask about the **specific issue** they are experiencing (e.g., too fast, too slow, or inconsistent tracking).
-4. Follow up with targeted questions to dig deeper into their gaming style and preferences:
+1. Begin by asking the user their **current DPI**. Include a choice for "Not sure" if they don't know.
+2. Always provide 4 choices along with the questions.
+3. Ask if they are satisfied with their current DPI. If they are, conclude the session by affirming their settings.
+4. If they are not satisfied, ask about the **specific issue** they are experiencing (e.g., too fast, too slow, or inconsistent tracking).
+5. Follow up with targeted questions to dig deeper into their gaming style and preferences:
    - The type of game they play and how they interact with it (e.g., fast reflex-based games, precision shooting, or strategy games).
    - The size of their mousepad or available space for movement.
    - Their preferred grip style (e.g., palm, claw, or fingertip grip).
@@ -25,22 +23,24 @@ PROMPT_LIBRARY = {
 - "What is your current DPI setting?"
   1. 800
   2. 1200
-  3. Not sure
+  3. 1600
+  4. Not sure
 
 - "Are you happy with your current DPI?"
   1. Yes, it feels just right.
   2. No, it feels too slow.
   3. No, it feels too fast.
+  4. No, it feels inconsistent.
 
 - "What issue do you face with your current DPI?"
   1. I overshoot targets often.
   2. I struggle to make precise movements.
   3. It feels inconsistent during gameplay.
+  4. Not sure.
 
 Continue asking relevant questions, always providing 3 well-thought-out options, and adapt based on the user's responses to recommend a suitable DPI range or additional adjustments they can try.
  """,
-"MACROS":
-"""You are a knowledgeable assistant specializing in gaming peripherals, particularly in setting up macros for gaming keyboards. Your objective is to guide users through a series of questions to understand their gaming needs and preferences, helping them create custom macros for enhanced gameplay.
+    "MACROS": """You are a knowledgeable assistant specializing in gaming peripherals, particularly in setting up macros for gaming keyboards. Your objective is to guide users through a series of questions to understand their gaming needs and preferences, helping them create custom macros for enhanced gameplay.
 
 You can choose from the following MACROS:
 - CUSTOM TEXT OR EMOJIS: Assign a specific text or emoji sequence to a key for quick communication or emote usage.
@@ -48,5 +48,5 @@ You can choose from the following MACROS:
 - STREAMING FUNCTIONS: Macros for starting/stopping streams, switching scenes, or adjusting audio levels.
 - GAME-SPECIFIC MACROS RECOMMENDATIONS: IN-GAME POPULAR MACROS FOR GAMES LIKE FORTNITE, APEX LEGENDS, ETC.
 - CUSTOM MACROS: Create your own custom macros for specific in-game actions or commands.
-"""
+""",
 }
--- main.py
+++ main.py
@@ -1,43 +1,43 @@
 from typing import List
 from pydantic import BaseModel
 import ollama
-
+from CONST import *
+import random
 
 class GHubQuestion(BaseModel):
     """
     JSON Schema for G Hub Question
     """
+
     question: str
-    choice_1: str
-    choice_2: str
-    choice_3: str
-    choice_4: str
-
-
-class DPISlots(BaseModel):
-    """
-    JSON Schema for DPI slots
-    """
-    dpi_1: int
-    dpi_2: int
-    dpi_3: int
-    dpi_4: int
-    dpi_5: int
+    choices: List[str]
 
 def format_question(question: object) -> str:
     """
     question: GHubQuestion object
     return: str
     """
-    return """Question : {question.question}\n
-        Choices\n
-        1. {question.choice_1}\n
-        2. {question.choice_2}\n
-        3. {question.choice_3}\n
-        4. {question.choice_4}\n""".format(question=question)
+    # return """Question : {question.question}\nChoices:\n1. {question.choice_1}\n2. {question.choice_2}\n3. {question.choice_3}\n4. {question.choice_4}\n""".format(question=question)
+    return f"Question: {question.question}\nChoices:\n" + "\n".join(question.choices)
+
+class DPISlots(BaseModel):
+    """
+    JSON Schema for DPI slots
+    """
+
+    dpi_1: int
+    dpi_2: int
+    dpi_3: int
+    dpi_4: int
+    dpi_5: int
 
 
-def generate_question(system_prompt, user_prompt, model_name="gemma2:2b", temperature=None):
+
+
+
+def generate_question(
+    system_prompt, user_prompt, model_name="gemma2:2b", temperature=None
+):
     """
     system_prompt: str
     user_prompt: str
@@ -49,68 +49,80 @@ def generate_question(system_prompt, user_prompt, model_name="gemma2:2b", temper
 
     if temperature:
         stream = ollama.chat(
-        model=model_name,
-        messages=[
-            {"role": "system", "content": system_prompt},
-            {"role": "user", "content": user_prompt}
-        ],
-        stream=False,
-        options={"temperature": temperature}
-    )
+            model=model_name,
+            messages=[
+                {"role": "system", "content": system_prompt},
+                {"role": "user", "content": user_prompt},
+            ],
+            stream=False,
+            options={"temperature": temperature},
+        )
     else:
         stream = ollama.chat(
             model=model_name,
             messages=[
                 {"role": "system", "content": system_prompt},
-                {"role": "user", "content": user_prompt}
+                {"role": "user", "content": user_prompt},
             ],
             stream=False,
         )
+    generated_text=stream["message"]["content"]
+    # print("###GENERATED TEXT###")
+    # print(generated_text)
+    # print("######"*10)
     response = ollama.chat(
         model=model_name,
         messages=[
-            {"role": "system", "content": system_prompt},
-            {"role": "user", "content": stream}
+            {"role": "system", "content": "Please format the text in the following method - Question:...\n Choices:\n...\n...\n...\n..."},
+            {"role": "user", "content": generated_text},
         ],
-        format=GHubQuestion.model_json_schema()
+        format=GHubQuestion.model_json_schema(),
     )
 
-    return GHubQuestion.model_validate_json(response['message']['content'])
+    return GHubQuestion.model_validate_json(response["message"]["content"])
 
 
-def generate_question_structured(system_prompt, user_prompt, model_name="gemma2:2b", temperature=None):
+def generate_question_structured(
+    system_prompt, user_prompt, model_name="gemma2:2b", temperature=None
+):
     """
-    This function generates a structured question for the user without the initial text generation step 
+    This function generates a structured question for the user without the initial text generation step
     system_prompt: str
     user_prompt: str
     user_context: str
     TEMPERATURE: float
-    """ 
+    """
     if temperature:
         stream = ollama.chat(
-        model=model_name,
-        messages=[
-            {"role": "system", "content": system_prompt},
-            {"role": "user", "content": user_prompt}
-        ],
-        stream=False,
-        format=GHubQuestion.model_json_schema(),
-        options={"temperature": temperature}
-    )
+            model=model_name,
+            messages=[
+                {"role": "system", "content": system_prompt},
+                {"role": "user", "content": user_prompt},
+            ],
+            format=GHubQuestion.model_json_schema(),
+            options={"temperature": temperature},
+        )
     else:
         stream = ollama.chat(
             model=model_name,
             messages=[
                 {"role": "system", "content": system_prompt},
-                {"role": "user", "content": user_prompt}
+                {"role": "user", "content": user_prompt},
             ],
-            stream=False,
-            format=GHubQuestion.model_json_schema()
+            format=GHubQuestion.model_json_schema(),
         )
 
-    return GHubQuestion.model_validate_json(stream['message']['content'])
+    generated_text=stream["message"]["content"]
+    return GHubQuestion.model_validate_json(generated_text)
 
-def evaluate_info(user_context:str, value_prompt:str, value_schema=object, model_name="gemma2:2b", temperature=None):
+
+def evaluate_info(
+    user_context: str,
+    value_prompt: str,
+    value_schema=object,
+    model_name="gemma2:2b",
+    temperature=None,
+):
     """
     user_context: str
     value_prompt: str
@@ -121,31 +133,77 @@ def evaluate_info(user_context:str, value_prompt:str, value_schema=object, model
     Returns: Object
     """
     if temperature:
-        stream = ollama.chat(
-        model=model_name,
-        messages=[
-            {"role": "system", "content": value_prompt.format(user_context=user_context)},
-        ],
-        stream=False,
-        options={"temperature": temperature}
-    )
+       stream = ollama.chat(
+            model=model_name,
+            messages=[
+                {
+                    "role": "system",
+                    "content": value_prompt.format(user_context=user_context),
+                },
+                {
+                    "role": "user",
+                    "content": "Suggest 5 DPI values for the user based on the context provided.",
+                },
+            ],
+            stream=False,
+            format=value_schema.model_json_schema(),
+            options={"temperature": temperature},
+        )
     else:
         stream = ollama.chat(
             model=model_name,
             messages=[
-                {"role": "user", "content": user_context},
-                {"role": "system", "content": value_prompt}
+                {
+                    "role": "system",
+                    "content": value_prompt.format(user_context=user_context),
+                },
+                {
+                    "role": "user",
+                    "content": "Suggest 5 DPI values for the user based on the context provided.",
+                },
             ],
             stream=False,
+            format=value_schema.model_json_schema(),
         )
-    response = ollama.chat(
-        model=model_name,
-        messages=[
-            {"role": "user", "content": user_context},
-            {"role": "system", "content": stream}
-        ],
-        format=value_schema.model_json_schema()
-    )
 
-    return value_schema.model_validate_json(response['message']['content'])
 
+    return value_schema.model_validate_json(stream["message"]["content"])
+
+
+if __name__ == "__main__":
+
+    system_prompt = PROMPT_LIBRARY["DPI"].format(game="Valorant")
+    user_prompt = PROMPT_LIBRARY["USER_PROMPT"]
+    model_name = "exaone3.5:2.4b"
+    NUM_QUESTIONS = 5
+    user_context = ""
+    updated_prompt = user_prompt
+
+    for i in range(NUM_QUESTIONS):
+        # Generate a question
+        print("############### Question {} ###############".format(i + 1))
+        question = generate_question_structured(system_prompt, updated_prompt, model_name)
+        question_str = format_question(question)
+        print(question_str)
+        user_context += question_str
+        x = random.randint(1, len(question.choices)) - 1
+        # if x == 1:
+        #     user_context += "Answer: {}\n".format(question.choice_1)
+        # elif x == 2:
+        #     user_context += "Answer: {}\n".format(question.choice_2)
+        # elif x == 3:
+        #     user_context += "Answer: {}\n".format(question.choice_3)
+        # else:
+        #     user_context += "Answer: {}\n".format(question.choice_4)
+
+        choice = question.choices[x]
+        print("Answer: ", choice)
+        user_context += "\nAnswer: {}\n".format(choice)
+        updated_prompt = user_context + "Ask another question buidling on the previous context to gain more relevant information.\nQuestion:"
+    
+    dpi_prompt = "Based on the following context: \n{user_context}\n"
+    dpi = evaluate_info(user_context, dpi_prompt, DPISlots, model_name)
+    print("############### DPI SLOTS ###############")
+    print(dpi)
+    # print("############### USER CONTEXT ###############")
+    # print(user_context)
